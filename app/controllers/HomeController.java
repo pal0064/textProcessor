e2f180ffb82dda9c68e16330401eba5419ecd67e
pal <pal@dhcp-10-132-153-249.uawifi.arizona.edu>
2022-08-27 07:55:15 -0700

    feat(core):  Added code to read text file

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -5,13 +5,18 @@ import play.i18n.Messages;
 import play.i18n.MessagesApi;
 import play.mvc.*;
 import views.html.index;
-
+import java.nio.file.Path;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.charset.StandardCharsets;
 /**
  * This class uses a custom body parser to change the upload type.
  */
@@ -41,14 +46,25 @@ public class HomeController extends Controller {
         final Http.MultipartFormData<File> formData = request.body().asMultipartFormData();
         final Http.MultipartFormData.FilePart<File> filePart = formData.getFile("name");
         final File file = filePart.getRef();
-        final long data = operateOnTempFile(file);
-        return ok("file size = " + data + "");
+        String content = null;
+        try {
+            content = readFile(file.toPath(),StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(content);
+         return ok(  content );
     }
 
     private long operateOnTempFile(File file) throws IOException {
         final long size = Files.size(file.toPath());
         Files.deleteIfExists(file.toPath());
         return size;
+    }
+
+    public static String readFile(Path filePath,Charset encoding) throws IOException {
+        return Files.readString(filePath, encoding);
     }
 
 }
