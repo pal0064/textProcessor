c56cdb54ae3702295ba4d1224e2b6cf2ddc0e1b4
pal <pal@dhcp-10-132-153-249.uawifi.arizona.edu>
2022-08-27 21:40:43 -0700

    feat(core): improved form

--- app/controllers/FormData.java
+++ app/controllers/FormData.java
@@ -1,17 +1,52 @@
 package controllers;
 
+import play.data.validation.Constraints;
+import play.libs.Files.TemporaryFile;
+import play.mvc.Http.MultipartFormData.FilePart;
+
+import java.io.File;
+
+/**
+ * A form processing DTO that maps to the widget form.
+ *
+ * Using a class specifically for form binding reduces the chances
+ * of a parameter tampering attack and makes code clearer, because
+ * you can define constraints against the class.
+ */
 public class FormData {
-    private String name;
 
-    public FormData() {}
+    private String inputText;
 
-    public FormData(String name) {
-        this.name = name;
+    private FilePart<TemporaryFile> inputFile;
+
+    private String outputType;
+
+
+    public FormData() {
     }
-    public String getName() {
-        return name;
+
+    public String getInputText() {
+        return inputText;
     }
-    public void setName(String name) {
-        this.name = name;
+
+    public void setInputText(String inputText) {
+        this.inputText = inputText;
     }
+
+    public FilePart<TemporaryFile> getInputFile() {
+        return inputFile;
+    }
+
+    public void setInputFile(FilePart<TemporaryFile> inputFile) {
+        this.inputFile = inputFile;
+    }
+
+
+    public String getOutputType() {
+        return outputType;
+    }
+    public void setOutputType(String outputType) {
+        this.outputType = outputType;
+    }
+
 }
--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -5,6 +5,8 @@ import edu.stanford.nlp.ling.CoreLabel;
 import edu.stanford.nlp.pipeline.CoreDocument;
 import edu.stanford.nlp.pipeline.CoreSentence;
 import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import play.data.Form;
 import play.i18n.Messages;
 import play.i18n.MessagesApi;
@@ -24,21 +26,22 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 
-import edu.stanford.nlp.simple.Token;
-import edu.stanford.nlp.simple.Document;
-import edu.stanford.nlp.simple.Sentence;
+import static play.libs.Scala.asScala;
+
 /**
  * This class uses a custom body parser to change the upload type.
  */
 @Singleton
 public class HomeController extends Controller {
+    private final Form<FormData> form;
     private final AssetsFinder assetsFinder;
     private final play.data.FormFactory formFactory;
     private MessagesApi messagesApi;
-
+    private final Logger logger = LoggerFactory.getLogger(getClass()) ;
     @Inject
     public HomeController(play.data.FormFactory formFactory, MessagesApi messagesApi,AssetsFinder assetsFinder) {
         this.formFactory = formFactory;
+        this.form = formFactory.form(FormData.class);
         this.messagesApi = messagesApi;
         this.assetsFinder = assetsFinder;
     }
@@ -48,72 +51,41 @@ public class HomeController extends Controller {
         Messages messages = this.messagesApi.preferred(request);
         return ok(index.render(form, request, messages));
     }
-
-
-    public Result downloadFile(Http.Request request) throws IOException{
-        System.out.println("download");
-        File csvFile = createCsvFile(request.id());
-        return ok(csvFile);
-    }
-
     /**
      * This method uses MyMultipartFormDataBodyParser as the body parser
      */
-    @BodyParser.Of(MyMultipartFormDataBodyParser.class)
-    public Result upload(Http.Request request) throws IOException {
-        final Http.MultipartFormData<File> formData = request.body().asMultipartFormData();
-        final Http.MultipartFormData.FilePart<File> filePart = formData.getFile("name");
-        final File file = filePart.getRef();
-        String content = null;
-        try {
-            content = readFile(file.toPath(),StandardCharsets.UTF_8);
-        } catch (IOException e) {
-            e.printStackTrace();
+//    @BodyParser.Of(MyMultipartFormDataBodyParser.class)
+    public Result submit(Http.Request request) throws IOException {
+//        final Http.MultipartFormData<File> formData = request.body().asMultipartFormData();
+//        File file = formData.getFile("file");
+        final Form<FormData> boundForm = form.bindFromRequest(request);
+        if (boundForm.hasErrors()) {
+            logger.error("errors = {}", boundForm.errors());
+            Messages messages = messagesApi.preferred(request);
+            return badRequest(index.render(boundForm, request, messages));
+        } else {
+            FormData data = boundForm.get();
+            return outputHandler(data);
         }
-        long numberOfSentences = 0;
-        long numberOfWords = 0;
-        long numberOfNouns = 0;
-        String NOUN_IDENTIFIER = "NN";
-        Properties props = new Properties();
-        props.setProperty("annotators", "tokenize,ssplit,pos");
-        props.setProperty("coref.algorithm", "neural");
-        StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
-        CoreDocument document = new CoreDocument(content);
-        pipeline.annotate(document);
-        for (CoreSentence sentence : document.sentences()) {
-            numberOfSentences+=1;
-            for( CoreLabel token : sentence.tokens()){
-                numberOfWords+=1;
-                if(token.tag().startsWith(NOUN_IDENTIFIER)){
-                    numberOfNouns+=1;
-                }
 
-            }
         }
-         String summary = String.join(
-                "\n",
-                 "\n\nSummary:\n",
-                "Number of Sentences : " + numberOfSentences,
-                "Number of Words : "+numberOfWords,
-                "Number of Nouns : " + numberOfNouns
-        );
-
-        return ok(views.html.summary.render(content + summary,assetsFinder));
-    }
 
 
-    private long operateOnTempFile(File file) throws IOException {
-        final long size = Files.size(file.toPath());
-        Files.deleteIfExists(file.toPath());
-        return size;
+
+    public Result  outputHandler(FormData data) throws IOException{
+        if ("FILE".equals(data.getOutputType())) {
+            return                  ok(createCsvFile());
+        }
+        return ok(views.html.summary.render(data.getInputText() + getSummary(data.getInputText()), assetsFinder));
+
     }
 
     public static String readFile(Path filePath,Charset encoding) throws IOException {
         return Files.readString(filePath, encoding);
     }
 
-    public static File createCsvFile(Long requestId) throws  IOException{
-        File file = File.createTempFile("summary_"+requestId, ".csv");
+    public static File createCsvFile() throws  IOException{
+        File file = File.createTempFile("summary_", ".csv");
         try {
             // create FileWriter object with file as parameter
             FileWriter outputFile = new FileWriter(file);
@@ -136,5 +108,52 @@ public class HomeController extends Controller {
         return  file;
     }
 
+public String getSummary(String inputText) {
+    long numberOfSentences = 0;
+    long numberOfWords = 0;
+    long numberOfNouns = 0;
+    String NOUN_IDENTIFIER = "NN";
+    Properties props = new Properties();
+    props.setProperty("annotators", "tokenize,ssplit,pos");
+    props.setProperty("coref.algorithm", "neural");
+    StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
+    CoreDocument document = new CoreDocument(inputText);
+    pipeline.annotate(document);
+    for (CoreSentence sentence : document.sentences()) {
+        numberOfSentences += 1;
+        for (CoreLabel token : sentence.tokens()) {
+            numberOfWords += 1;
+            if (token.tag().startsWith(NOUN_IDENTIFIER)) {
+                numberOfNouns += 1;
+            }
 
+        }
+    }
+    String summary = String.join(
+            "\n",
+            "\n\nSummary:\n",
+            "Number of Sentences : " + numberOfSentences,
+            "Number of Words : " + numberOfWords,
+            "Number of Nouns : " + numberOfNouns
+    );
+    return summary;
+
+}
+
+
+
+//    @BodyParser.Of(value = BodyParser.Text.class, maxLength = 10 * 1024)
+//    public static Result upload() {
+//
+//        FilePart picture = body.getFile("picture");
+//        if (picture != null) {
+//            String fileName = picture.getFilename();
+//            String contentType = picture.getContentType();
+//            File file = picture.getFile();
+//            return ok("File uploaded");
+//        } else {
+//            flash("error", "Missing file");
+//            return redirect(routes.Application.index());
+//        }
+//    }
     }
